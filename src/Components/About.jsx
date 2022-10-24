dce94dec3ee563592ad3691b7999544de41cf9ee
Vishnu <vishnujishnu0987@gmail.com>
2022-10-24 11:01:52 -0800

    added some effects

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,10 +5,10 @@ function About() {
   return (
     <div className='bg-gradient-to-b from-[#000300] to-[#0f1e26] ' id='about'>
         <div className=' bg-transparent pt-16 grid md:grid-cols-2 pb-16 '>
-            <img src={image}alt="/" className='mx-auto max-w-[300px]  rounded-full md:max-w-[400px] md:rounded-2xl md:mx-auto' />
-            <div className='bg-transparent'>
+            <img  src={image}alt="/" className='duration-300 ease-in-out mx-auto max-w-[300px]  rounded-full md:max-w-[400px] md:rounded-2xl md:mx-auto'/>
+            <div className='bg-transparent blur-none'>
                 <h1 className='text-white bg-transparent m-4 text-3xl text-center  '>About Me</h1>
-                <p className='text-white bg-transparent text-center m-4 md:mt-20 md:mr-8'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <p className='text-white bg-transparent text-center m-4 md:mt-20 md:mr-8'>I am vishnu v. I am a front end developer.</p>
                 <button className='text-white flex justify-center mx-auto  bg-gradient-to-br from-[#40d9bd] to-[#4ad29a] rounded-xl w-[200px] h-[50px] p-3 '>Download CV</button>
             </div>       
         </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,27 +2,29 @@ import React from 'react'
 import olx from "../assets/olx.png"
 import netflix from "../assets/netflix.png"
 import todo from "../assets/todo.png"
+import {useInView} from "react-intersection-observer"
 
 function Projects() {
-  return (
+    const {ref,inView}=useInView( {threshold:0.5})
+  return(
     <div className='bg-gradient-to-b from-[#0f1e26] to-[#000300]' id='projects'>
         <h1 className='text-white bg-transparent text-center text-5xl font-bold'>Projects</h1>
         <div className='bg-transparent grid md:grid-cols-2 lg:grid-cols-3'>
-            <div className=' bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg'>
+            <div ref={ref} className={inView?' bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg  ease-in-out opacity-100':" bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg lg:translate-x-[-200px] lg:opacity-0"}>
                 <img className='rounded-xl' src={olx} alt="/" />
                 <h1 className='bg-transparent text-white text-center mx-auto justifiy-center font-bold mt-2'>OLX CLONE</h1>
                 <h1 className='bg-transparent text-white'>Description :</h1>
                 <p className='bg-transparent text-white'>Web app which is almost similar to olx made using react and firebase</p>
                 <button className='bg-transparent text-[#4ad2b2] text-center mx-auto justify-center mt-4 m  focus:border-green-300 hover:text-green-200' onClick={()=>{ window.open("https://github.com/VishnuKaniyamvayal/Olx-clone-")}}>Go to code</button>
             </div>
-            <div className=' bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg'>
+            <div ref={ref} className={inView?' bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg ease-in-out opacity-100':" bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg lg:translate-x-[-200px] lg:opacity-0"}>
                 <img className='rounded-xl' src={netflix} alt="/" />
                 <h1 className='bg-transparent text-white text-center mx-auto justifiy-center font-bold mt-2'>NETFLIX CLONE</h1>
                 <h1 className='bg-transparent text-white'>Description :</h1>
                 <p className='bg-transparent text-white'>Project not completed due to some API issues. the hero section is still dynamic</p>
                 <button className='bg-transparent text-[#4ad2b2] text-center mx-auto justify-center mt-4 m  focus:border-green-300 hover:text-green-200' onClick={()=>{window.open("https://github.com/VishnuKaniyamvayal/Netflix-clone")}}>Go to code</button>
             </div>
-            <div className=' bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg'>
+            <div ref={ref} className={inView?' bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg ease-in-out opacity-100':" bg-transparent p-10 border rounded-xl m-6 mt-10 border-gray-700 hover:scale-105 duration-300 shadow-lg lg:translate-x-[-200px] lg:opacity-0"}>
                 <img className='rounded-xl' src={todo} alt="/" />
                 <h1 className='bg-transparent text-white text-center mx-auto justifiy-center font-bold mt-2'>TODO APP</h1>
                 <h1 className='bg-transparent text-white'>Description :</h1>
