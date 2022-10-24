885a8a8b2d05526083c08f6575a95b32b3f76018
Vishnu <vishnujishnu0987@gmail.com>
2022-10-24 11:21:17 -0800

    just updated about me section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ function About() {
             <img  src={image}alt="/" className='duration-300 ease-in-out mx-auto max-w-[300px]  rounded-full md:max-w-[400px] md:rounded-2xl md:mx-auto'/>
             <div className='bg-transparent blur-none'>
                 <h1 className='text-white bg-transparent m-4 text-3xl text-center  '>About Me</h1>
-                <p className='text-white bg-transparent text-center m-4 md:mt-20 md:mr-8'>I am vishnu v. I am a front end developer.</p>
+                <p className='text-white bg-transparent text-center m-4 md:mt-20 md:mr-8'>I am vishnu v. I am a front end developer, i am currently doing my engineering degree in computer science.I am always curious to learn new stuff.I got skills instead of certificates</p>
                 <button className='text-white flex justify-center mx-auto  bg-gradient-to-br from-[#40d9bd] to-[#4ad29a] rounded-xl w-[200px] h-[50px] p-3 '>Download CV</button>
             </div>       
         </div>
