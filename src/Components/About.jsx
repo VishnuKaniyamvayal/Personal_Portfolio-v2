aefa209fd12addc090da67a9e2ea192a673ef3c0
Vishnu <vishnujishnu0987@gmail.com>
2022-12-20 08:30:21 -0800

    resume link and resume updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ function About() {
             <div className='bg-transparent blur-none'>
                 <h1 className='text-white bg-transparent m-4 text-3xl text-center  '>About Me</h1>
                 <p className='text-white bg-transparent text-center m-4 md:mt-20 md:mr-8'>I am vishnu v. I am a front end developer, i am currently doing my engineering degree in computer science.I am always curious to learn new stuff.I got skills instead of certificates</p>
-                <button onClick={()=>{window.open("https://drive.google.com/u/0/uc?id=1JHvqrqcJ714GOYw1024qf82owjzATUeP&export=download")}} className='text-white flex justify-center mx-auto  bg-gradient-to-br from-[#40d9bd] to-[#4ad29a] rounded-xl w-[200px] h-[50px] p-3 '>Download CV</button>
+                <button onClick={()=>{window.open("http://drive.google.com/uc?export=download&id=1LXMx9eaU8bqnyJdB0gu7Pkyd9GI9eEDD")}} className='text-white flex justify-center mx-auto  bg-gradient-to-br from-[#40d9bd] to-[#4ad29a] rounded-xl w-[200px] h-[50px] p-3 '>Download CV</button>
             </div>       
         </div>
 
