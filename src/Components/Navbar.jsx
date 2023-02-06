f8fe160c0ecb2c25fe7513acc9cc01ce57859014
Vishnu <vishnujishnu0987@gmail.com>
2023-02-06 15:30:57 -0800

    minor bugs cleared

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -41,11 +41,11 @@ function Navbar() {
             <li className={`bg-transparent p-4 cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}><Link className={`bg-transparent p-4 cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}  to="about" spy={true} smooth={true} offset={-250} duration={500} >About me</Link></li>
             <li className={`bg-transparent p-4 cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}><Link className={`bg-transparent p-4 cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}  to="projects" spy={true} smooth={true} offset={-50} duration={500} >Projects</Link></li>
             <li className={`bg-transparent p-4 cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}><Link className={`bg-transparent p-4 cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `} to="contact" spy={true} smooth={true} offset={50} duration={500} >Contact me</Link></li>
-            <button className='mr-[-30px] ' onClick={dark?themeChangeToLight:themeChangeToDark}><li className={`bg-transparent p-4 cursor-pointer pl-6 ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}>{dark?<MdLightMode size={23}/>:<BsFillMoonFill size={23} />}</li></button>
+            <button className='pr-[-30px] ' onClick={dark?themeChangeToLight:themeChangeToDark}><li className={`bg-transparent p-4 cursor-pointer pl-6 ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}>{dark?<MdLightMode size={23}/>:<BsFillMoonFill size={23} />}</li></button>
             
         </ul>
         <div  className='text-white md:hidden  flex'>
-        <button className=' ml-6' onClick={dark?themeChangeToLight:themeChangeToDark}><li className={`mr-12 list-none bg-transparent cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}>{dark?<MdLightMode size={23}/>:<BsFillMoonFill size={23} />}</li></button>
+        <button className=' mr-6' onClick={dark?themeChangeToLight:themeChangeToDark}><li className={`mr-12 list-none bg-transparent cursor-pointer ${dark?"text-white hover:text-[#6FEDD6]":"text-gray-500 hover:text-black"} `}>{dark?<MdLightMode size={23}/>:<BsFillMoonFill size={23} />}</li></button>
         {!nav?<RiMenu3Fill onClick={()=>{handleNav()}} className={`mt-1 mr-2 bg-transparent right-2 ${dark? " text-gray-400":" text-gray-500"}`} size="20px"/>:<AiOutlineClose onClick={()=>{handleNav()}} className={`fixed  bg-transparent right-2 ${dark?"bg-[#00300] text-gray-400":"bg-white text-gray-500"}`}/>}
         </div>
         
