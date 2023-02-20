ae09d016ee7dc355c3513744cc85f82ddcce1e93
Vishnu v <111561574+VishnuKaniyamvayal@users.noreply.github.com>
2023-02-20 09:22:55 +0530

    Changed resume link

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,8 +15,8 @@ function About() {
             <img  src={image}alt="/" className='duration-300 ease-in-out mx-auto max-w-[300px]  rounded-full md:max-w-[400px] md:rounded-2xl md:mx-auto'/>
             <div className='bg-transparent blur-none'>
                 <h1 className={`${dark?"text-white":"text-black"} bg-transparent m-4 text-3xl text-center`}>About Me</h1>
-                <p className={`${dark?"text-white":"text-black"} bg-transparent text-center m-4 md:mt-20 md:mr-8`}>Just a common man who got some skills</p>
-                <button onClick={()=>{window.open("http://drive.google.com/uc?export=download&id=1LXMx9eaU8bqnyJdB0gu7Pkyd9GI9eEDD")}} className='text-white flex justify-center mx-auto  bg-gradient-to-br from-[#40d9bd] to-[#4ad29a] rounded-xl w-[200px] h-[50px] p-3 '>Download CV</button>
+                <p className={`${dark?"text-white":"text-black"} bg-transparent text-center m-4 md:mt-20 md:mr-8`}>Self thaught React developer</p>
+                <button onClick={()=>{window.open("https://1drv.ms/b/s!AulwaNCvuBo2gwDb2qnkYwMlW_6w")}} className='text-white flex justify-center mx-auto  bg-gradient-to-br from-[#40d9bd] to-[#4ad29a] rounded-xl w-[200px] h-[50px] p-3 '>Download CV</button>
             </div>       
         </div>
 
